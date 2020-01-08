01c520b1096a44c5e9202a958324509792f11103
dev_nullified <3087935+dev-nullified@users.noreply.github.com>
2020-01-07 18:12:08 -0800

    Added basic Navbar component and DB connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,33 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sassutils.wsgi import SassMiddleware
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
 
-# Load the views
-from app import views
 
 # Load the config file
 app.config.from_object('config')
+
+# Configure SaaS compile
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': ('static/styles', 'static/css', '/static/css')
+})
+
+# DB config
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+# prevent cached responses
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# VERY LAST import
+from app import views, models
--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 # config.py
+import os
 
 # Enable Flask's debugging features. Should be False in production
 DEBUG = True
+
+
+# DB connection
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres+psycopg2://postgres:password@localhost:5432/VMCTAP'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
