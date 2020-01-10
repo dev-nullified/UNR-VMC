a2390e3a3a04d8403e7e0df4325aeff11c6df953
Autumn <acuellar24@nevada.unr.edu>
2020-01-09 20:06:15 -0800

    updated view

--- app/views.py
+++ app/views.py
@@ -1,10 +1,59 @@
-from flask import render_template
+from flask import render_template, redirect
 from app import app
+
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    if True:
+        return redirect('/login')
+    else:
+        return render_template("index.html")
 
+
+@app.route('/login', methods=['GET'])
+def login():
+    pass
+
+@app.route('/logout')
+def logout():
+    # unathenticate
+    return redirect('/')
+
+@app.route('/students', methods=['GET'])
+def students():
+    pass
+
+@app.route('/student/<int: id>', methods=['GET'])
+def student(id):
+    pass
+
+@app.route('/report', methods=['GET'])
+def report():
+    pass
+
+@app.route('/create', methods=['PUT', 'GET'])
+def create():
+    pass
+
+@app.route('/view/<int: id>', methods=['GET'])
+def view(id):
+    pass
+
+@app.route('/delete/<int: id>', methods=['DELETE'])
+def delete(id):
+    pass
+
+@app.route('/import', methods=['PUT', 'GET'])
+def upload():
+    pass
+
+@app.route('/nofications')
+def nofications():
+    pass
+
+@app.route('/stats', methods=['GET'])
+def stats():
+    pass
 
 @app.route('/about')
 def about():
